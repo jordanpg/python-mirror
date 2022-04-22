900aa9eb21b1e1b5c090b62c500113ea87464348
Jordan Gilbreath <jpg0016@uah.edu>
2022-04-21 23:01:56 -0500

    just merge it all. WHo cares

--- pymirror/mirror.py
+++ pymirror/mirror.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
-from pymirror.cpu import Cpu
-from pymirror.transaction import Transaction
-from pymirror.resource import Resource
+from enum import Enum
+from heapq import heapify, heappop, heappush
+import random
+from typing import Union
 
 @dataclass
 class MirrorOptions:
@@ -9,31 +10,301 @@ class MirrorOptions:
     db_size = 1000  # Number of resources to generate
     replicas = 4    # Number of replicas for each resource
     cpu_count = 8   # Number of CPUs in the system
-    
+
     # Timing settings
     access_time = 20        # Number of ticks to access a data item on disk (assumed to be the case)
     buffered_time = 10      # Number of ticks to access a data item in the buffer pool
     buffered_chance = 0.1   # Probability of using buffered_time instead of access_time
     write_time = 2          # Number of ticks to initiate a write
     spawn_time = 1          # Number of ticks to spawn a remote updater
-    
+
     # Transaction settings
     arrival_rate = 16           # Number of transactions expected per 1000 ticks
     deadline_slack = 6          # Multiplier for expected transaction length
     transaction_size = (5,16)   # Range of possible transaction sizes (i.e., number of processes spawned)
     write_chance = 0.25         # Probability that a spawned process will require write access
-    
 
 class Mirror:
     """Coordinator for the MIRROR simulation"""
     default_options = MirrorOptions()
-    def __init__(self, options: MirrorOptions = default_options) -> None:
+    @staticmethod
+    def pa_pb(holder: 'Process', requestor: 'Process') -> bool:
+        """Defines the PA_PB process used to solve lock conflicts. Returns True if holder should be aborted."""
+        # For updaters, block if the process has acquired all their local locks
+        if holder.is_updater:
+            if holder.lock is None:
+                return holder.priority < requestor.priority
+            return False
+        # For workers, block if the process has entered the Contract state
+        if holder.state.value >= ProcessState.Contract:
+            return False
+        
+        # Otherwise, use priority abort and abort the holder if its priority is lower.
+        return holder.priority < requestor.priority
+        
+    def __init__(self, options: MirrorOptions = default_options, method = pa_pb) -> None:
         self.options = options
+        self.clock = 0
         self.transactions: list[Transaction] = []
         self.cpus = [Cpu() for _ in range(options.cpu_count)] # Initialize CPUs
-        self.resources = [Resource(options.replicas) for _ in range(options.db_size)] # Initialize resources
+        self.resources = [Resource(i, options.replicas) for i in range(options.db_size)] # Initialize resources
+        self.method = method
         
     def tick(self):
         """Progress the entire simulation by one step"""
+        self.clock += 1
         for cpu in self.cpus:
             cpu.tick()
+            
+    def submit_job(self, p: 'Process') -> 'Cpu':
+        """Assign a job to a random CPU"""
+        cpu = random.choice(self.cpus)
+        cpu.schedule(p)
+        return cpu
+
+class Transaction:
+    """A transaction models a database transaction, spawning processes and awaiting their completion"""
+    def __init__(self, sim: Mirror, curr_time: int) -> None:
+        self.sim = sim
+        self.processes: list[Process] = []
+        self.deadline = curr_time
+        
+    def commit(self, p: 'Process'):
+        if p not in self.processes:
+            return
+        
+        if all([i.state == ProcessState.Complete for i in self.processes]):
+            print("Finished")
+
+class ProcessType(Enum):
+    Worker=0b001 # A worker will finish its progress then enter Contract state, and then complete if it is not a writer.
+    Writer=0b011 # A writer will spawn updaters for all other copies once entering the Contract state, then complete when each updater is ready.
+    Updater=0b100 # An updater will acquire a lock, initiate a write, and then enter Contract state, and complete when signaled by the writer.
+
+class ProcessState(Enum):
+    Begin=0     # Initial state, before the process arrives at the resource
+    Expand=1    # Waiting/working state
+    Contract=2  # Work finished, ready to commit or complete
+    Complete=3  # Process is done.
+
+class Process:
+    """A process models a working unit of a transaction"""
+    def __init__(self, owner: Transaction, target: 'Resource', length: int, type: ProcessType) -> None:
+        self.owner = owner
+        self.arrival = owner.sim.clock
+        self.length = length
+        self.add_length = 0 # Amount of additional work incurred during execution.
+        self.progress = 0 # Number of ticks spent on current task
+        self.type = type
+        self.state = ProcessState.Begin
+        self.target = target
+        self.updaters: list[Process] = []
+        self.lock: Union[Lock, None] = None
+        
+    def tick(self) -> bool:
+        """Progress this process. Returns True if the process has completed."""
+        if self.state is not ProcessState.Contract:
+            # Register with resource 
+            if self.state is ProcessState.Begin:
+                self.target.acquire(self) # Request a lock on the resource
+                self.state = ProcessState.Expand
+            
+            # If we aren't blocking, then do some work
+            if self.lock is not None:
+                self.progress += 1
+                if self.progress >= self.length:
+                    return self.ready()
+        else:
+            # If this process is currently spawning updaters, increase progress.
+            if self.spawning:
+                self.progress += 1
+                # If we should create another updater, create it and then start working on the next if needed.
+                if self.progress >= self.length:
+                    self.spawn_updater()
+                    # If we still need more updaters, increase length again.
+                    if self.spawning:
+                        self.length += self.owner.sim.options.spawn_time
+                    return False
+            elif self.is_writer:
+                # If all updaters are finished
+                for i in self.updaters:
+                    if i.state is not ProcessState.Contract:
+                        return False
+                return self.complete()
+                    
+        return False
+                
+    def ready(self):
+        """Ready this process and do any subsequent work. Returns True if the process has completed."""
+        self.state = ProcessState.Contract
+        
+        # If we are just a worker, then we no changes to commit and can simply release our lock and finish.
+        if self.is_worker:
+            return self.complete()
+        # If we are a writer, then we need to start working on spawning updaters.
+        if self.is_writer:
+            # If we don't need to spawn any updaters, then finish here.
+            if not self.spawning:
+                return self.complete()
+            # If spawn_time is configured as 0 or less, just instantly create all updaters.
+            if self.owner.sim.options.spawn_time < 1:
+                while self.spawning:
+                    self.spawn_updater()
+                return False
+
+            self.length += self.owner.sim.options.spawn_time
+            
+        return False
+    
+    def spawn_updater(self):
+        """Create an updater to acquire a lock for the resource"""
+        updater = Process(self.owner, self.target, self.owner.sim.options.write_time, ProcessType.Updater)
+        self.updaters.append(updater)
+    
+    def complete(self):
+        """Complete and release any locks held by this process or its updaters. The commit phase is presumed to occur here as well."""
+        self.state = ProcessState.Complete
+        self.release_all()
+        self.owner.commit(self)
+        return True
+    
+    def release_all(self):
+        """Release any held locks by this process or its updaters and exit blocking queues"""
+        self.target.release(self)
+        for u in self.updaters:
+            self.target.release(u)
+            u.complete()
+    
+    @property
+    def priority(self):
+        return self.owner.deadline
+    
+    @property
+    def is_worker(self):
+        return self.type == ProcessType.Worker
+    
+    @property
+    def is_writer(self):
+        return self.type == ProcessType.Writer
+    
+    @property
+    def is_updater(self):
+        return self.type == ProcessType.Updater
+    
+    @property
+    def spawning(self):
+        if not self.is_writer:
+            return False
+        if self.state is not ProcessState.Contract:
+            return False
+        return len(self.updaters) < (len(self.target.copies) - 1)
+    
+    @property
+    def blocking(self):
+        if self.state != ProcessState.Begin and not self.lock:
+            return True
+        if self.is_writer and not self.spawning:
+            for u in self.updaters:
+                if u.state.value < ProcessState.Contract.value:
+                    return True
+                
+        return False
+    
+    def __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, Process):
+            return False
+        if self.priority == __o.priority:
+            return self.arrival == __o.arrival
+        return self.priority == __o.priority
+    
+    def __lt__(self, __o: object) -> bool:
+        if not isinstance(__o, Process):
+            return False
+        if self.priority == __o.priority:
+            return self.arrival < __o.arrival
+        return self.priority < __o.priority
+    
+class Cpu:
+    """A CPU propagates ticks to one process at a time"""
+    def __init__(self) -> None:
+        self.processes: list[tuple[int, Process]] = [] # Initialize heap
+        
+    def schedule(self, p: Process):
+        """Schedule a process to this CPU"""
+        # Add this process to the heap, using the transaction deadline as the key value
+        heappush(self.processes, (p.priority, p))
+        
+    def deschedule(self, p: Process):
+        j = (p.priority, p)
+        if j not in self.processes:
+            return
+        self.processes.remove((p.priority, p))
+        heapify(self.processes)
+        
+    def tick(self):
+        """Propagate tick to running process"""
+        if len(self.processes) < 1:
+            return
+
+        ind = self.running_index()
+        if ind < 0: # All processes are blocking! 
+            return
+        
+        # Tick running process, then remove it if it has completed.
+        job = self.processes[ind]
+        if job[1].tick():
+            self.processes.remove(job)
+            heapify(self.processes)
+            
+    def running_index(self):
+        """Determine which process should be running"""
+        for ind,(_,p) in enumerate(self.processes):
+            if p.state is ProcessState.Begin: # Always tick a process if it is still in the Begin phase
+                return ind
+            elif p.blocking: # Skip processes that are blocking
+                continue
+            return ind
+        return -1
+
+@dataclass
+class Lock:
+    """A lock provides restricted access to a copy of a resource"""
+    holder: Process = None
+
+class Resource:
+    """A Resource models all copies of a resource across all sites, represented as a set of locks"""
+    def __init__(self, id: int, num_copies: int) -> None:
+        self.id = id
+        self.copies = [Lock() for _ in range(num_copies)]
+        self.queue: list[tuple[int, Process]] = [] # Blocking queue
+        
+    def acquire(self, p: Process) -> bool:
+        """Acquire a lock to this resource or enter queue. Returns True if a lock is acquired immediately."""
+        for c in self.copies:
+            if c.holder is None:
+                p.lock = c
+                c.holder = p
+                return True
+        heappush(self.queue, (p.priority, p))
+        return False
+    
+    def release(self, p: Process):
+        """Release a lock held by this process or exit queue"""
+        for c in self.copies:
+            if c.holder is p:
+                p.lock = None
+                # If the queue is empty, then just leave the lock free.
+                if len(self.queue) < 1:
+                    c.holder = None
+                    return
+                # Pass the lock to the next process in the queue
+                _, np = heappop(self.queue)
+                np.lock = c
+                c.holder = np
+                return
+        # Remove this process from the queue
+        j = (p.priority, p)
+        if j not in self.queue:
+            return
+        self.queue.remove((p.priority, p))
+        heapify(self.queue)
---
+++ test.py
@@ -0,0 +1,44 @@
+from collections import namedtuple
+from pymirror.mirror import *
+
+def writer():
+    opt = MirrorOptions()
+    opt.replicas = 2
+    m = Mirror(opt)
+    r = m.resources[0]
+    c = m.cpus[0]
+    t = Transaction(m, 0)
+    p = Process(t, r, 10, ProcessType.Writer)
+    t.processes.append(p)
+    
+    c.schedule(p)
+    assert p.lock is None
+    assert p.state is ProcessState.Begin
+    m.tick()
+    assert p.lock is r.copies[0]
+    assert p.state is ProcessState.Expand
+    assert p.progress == 1
+    for _ in range(9):
+        m.tick()
+    assert p.progress == 10
+    assert p.spawning
+    assert p.state is (ProcessState.Complete if p.type is ProcessType.Worker else ProcessState.Contract)
+    assert not p.blocking
+    m.tick()
+    assert p.blocking
+    u = p.updaters[0]
+    c.schedule(u)
+    assert u.lock is None
+    assert any([l.holder is None for l in r.copies])
+    m.tick()
+    assert u.lock is not None
+    assert u.progress == 1
+    assert all([l.holder for l in r.copies])
+    m.tick()
+    assert u.state == ProcessState.Contract
+    m.tick()
+    assert u.state == ProcessState.Complete
+    assert p.state == ProcessState.Complete
+
+if __name__ == "__main__":
+    writer()
