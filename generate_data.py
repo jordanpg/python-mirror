11828b58c0e958f083b0276fe169e4ad3031e545
Jordan Gilbreath <jpg0016@uah.edu>
2022-04-22 23:06:36 -0500

    testing

--- generate_data.py
+++ generate_data.py
@@ -1,4 +1,5 @@
 import csv
+from os.path import exists
 from pymirror.mirror import *
 
 def run_test(cpus: int, arrival_rate: int, ops: MirrorOptions = None):
@@ -23,21 +24,34 @@ def run_test(cpus: int, arrival_rate: int, ops: MirrorOptions = None):
     }
 
 if __name__ == "__main__":
+    prev_datafile = 'results.csv'
+    datafile = 'results.csv'
     cpu_tests = [8, 16, 32]
-    arrival_rate_tests = range(5,76,5)
+    arrival_rate_tests = range(20,151,5)
+    
+    # Load existing results if they exist
     results = []
+    if exists(prev_datafile):
+        print(f"Reading previous results from {prev_datafile}...")
+        with open(prev_datafile, 'r') as file:
+            reader = csv.DictReader(file)
+            results.extend(reader)
     
     ops_template = MirrorOptions()
     
-    for cpus in cpu_tests:
-        for arrival_rate in arrival_rate_tests:
-            print(f'Running test: {cpus} CPUs, {arrival_rate} transactions/1000 cycles')
-            results.append(run_test(cpus, arrival_rate, ops_template))
-    
-    print("Writing results to results.csv...")
-    with open("results.csv", 'w') as file:
+    print(f"Writing results to {datafile}...")
+    with open(datafile, 'w') as file:
         fields = ['cpus', 'arrival_rate', 'cycles', 'num_finished', 'num_missed', 'miss_pct', 'idle_cycles', 'cc_aborts']
         dw = csv.DictWriter(file, fieldnames=fields)
         
         dw.writeheader()
         dw.writerows(results)
+        
+        for cpus in cpu_tests:
+            for arrival_rate in arrival_rate_tests:
+                print(f'Running test: {cpus} CPUs, {arrival_rate} transactions/1000 cycles')
+                test = run_test(cpus, arrival_rate, ops_template)
+                dw.writerow(test)
+                # Stop testing this CPU if the miss percentage has exceeded 90%
+                if test["miss_pct"] > 0.9:
+                    continue
